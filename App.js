6469172745ff0496c651af32aaf9bcbe5f0925ec
bilel ayachi <daskik75@gmail.com>
2020-08-14 17:51:08 +0200

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, FlatList, View, Text, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      countries: [] }
+  }
+
+  renderItem = ({item}) => {
+    console.log('#item', item)
+    return(
+      <View>
+        <Text>
+          {item.name}
+        </Text>
+        <Text>
+          {item.capital}
+        </Text>
+      </View>
+      
+    )
+    
+      
+  }
+
+  componentDidMount () {
+    const url = 'http://restcountries.eu/rest/v2/all';
+    fetch(url)
+    .then((res) => res.json())
+    .then((resjson) => 
+      this.setState({
+        countries: resjson
+      })
+      // console.log(resjson)
+    )
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+        data={this.state.countries}
+        renderItem={this.renderItem}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    flex: 1
+  }
+})
+
+export default App;
